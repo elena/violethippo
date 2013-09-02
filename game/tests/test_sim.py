8de50499c0a5c583e0ea92ab4f5c95fe7f406c35
Richard Jones <richard@mechanicalcat.net>
2013-09-03 09:09:55 +1000

    make a test; though more assertions are needed

--- game/tests/test_sim.py
+++ game/tests/test_sim.py
@@ -11,21 +11,16 @@ from gamelib import model,player_orders,group_plans
 
 
 class FakeUI:
-
-    class SIGNAL_GAMEOVER(Exception): pass
+    class SIGNAL_GAMEOVER(Exception):
+        pass
 
     def __init__(self, savedir):
         self.savedir = savedir
         self.logging_begin(savedir)
-        self.fd = sys.stderr
+        self.messages = []
 
     def msg(self, msg, *args):
-        if msg:
-            try:
-                msg = msg % args
-            except:
-                msg = '%s %s' % (msg, args)
-        self.fd.write(msg + '\n')
+        self.messages.append((msg, args))
 
     def logging_begin(self, sdir):
         if os.path.exists(sdir):
@@ -33,10 +28,12 @@ class FakeUI:
         os.mkdir(sdir)
 
 
-def main(testfile):
+def test_model_construction():
     g = model.Game()
-    random.seed(283274823.2384701)
+    random.seed(1)
+
     g.moon.zones.append(model.Zone('Industry'))
+
     f = g.moon.zones[0].faction = model.Faction('baddies')
     f.threat = .25
     f.size = .1
@@ -44,7 +41,8 @@ def main(testfile):
     f.smart = .1
     f.loyal = .5
     f.rich = .5
-    f.buffs = ['military', 'military', 'fantic']
+    f.buffs = ['military', 'military', 'fanatic']
+
     r = model.Resistance('managers')
     f.size = .2
     f.informed = .1
@@ -53,6 +51,7 @@ def main(testfile):
     f.rich = .4
     f.buffs = ['leader']
     g.moon.zones[0].resistance_groups.append(r)
+
     r = model.Resistance('workers')
     f.size = .4
     f.informed = .2
@@ -67,22 +66,25 @@ def main(testfile):
     c.liberty = .4
     c.quality_of_life = .2
     c.cash = .4
-    c = g.moon.zones[0].cohorts[0]
+
+    c = g.moon.zones[0].cohorts[1]
     c.size = .4
     c.liberty = .2
     c.quality_of_life = .1
     c.cash = .2
 
-    ui = FakeUI('save.tmp')
+    ui = FakeUI('save.test')
     g.update(ui)
 
     g2 = g.json_loadfile(ui.savedir)
     g2.json_savefile(ui.savedir, 'save.json.verify')
     # diffing save.json and save.json.verify should be equal...
 
+    with open('save.test/save.json') as f1, open('save.test/save.json.verify') as f2:
+        assert f1.read() == f2.read()
+
     g.update(ui)
     g.update(ui)
     g.update(ui)
 
-if __name__ =  = "__main__":
-    main('test00.txt')
+    shutil.rmtree('save.test')
