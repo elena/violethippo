095916a8ece5bf36e8d342181ab316b837b4ae9c
Cameron Blackwood <cameron.blackwood@gmail.com>
2013-09-03 04:54:42 +1000

    added save to json, load json, some start of a turn process. added simtest directory for simulating turns quickly

---
+++ game/gamelib/group_plans.py
@@ -0,0 +1,24 @@
+
+'''
+Patrol - reduce rebellious this turn
+Root out Resistance - search for most visible resistance in a Cohort; prevents visibility from dropping this turn for other Groups in the Cohort?
+Assault Known Resistance Group - direct attack
+Upgrade equipment - increase Smarts
+Increase Funding
+Suppress cohort - reduce liberty (to increase willingness) for a turn/permanently
+Alter cohort stats - various deliberate changes possible, with fallout for each
+Recruit? - not sure if this should be possible, other than to recover what was lost
+Defend particular target (eg. production, or a cohort, or the Faction) - reduce chance of success of relevant actions this turn, increase visibility cost
+Informants - intel on planned actions for next turn (and/or targets)
+Search for player
+'''
+
+# note: Order vs Plan (plans are what NPCs do)
+class Plan:
+    """NPC group plans"""
+    pass
+
+
+class Patrol(Plan):
+    """Increases difficulty of resistance groups operations
+    """
--- game/gamelib/model.py
+++ game/gamelib/model.py
@@ -1,34 +1,101 @@
-class Game:
+
+import os
+import json
+
+from gamelib.plans.base import Plan
+
+
+class JSONable(object):
+
+    # take a list of simple variables to save....
+    def json_dump_simple(self,*names):
+        v={}
+        for k in names:
+            v['.'+k]=getattr(self,k)
+        return v
+    # restore anything simple.....
+    def json_load_simple(self,jdata):
+        v={}
+        for k in jdata:
+            if k.startswith('.'):
+                v[k]=setattr(self,k[1:],jdata[k])
+        return v
+
+    # create an object from a json save.
+    @classmethod
+    def json_create(cls, jdata):
+        o = cls( *cls.json_create_args(jdata) )
+        o.json_load_simple(jdata)
+        o.json_load(jdata)
+        return o
+    # handle __init__ args
+    @classmethod
+    def json_create_args(cls,jdata):
+        return []
+    # then load the extra (non simple) data bits... default to nothing to load
+    def json_load(self,jdata):
+        pass
+
+
+
+
+
+class Game(JSONable):
     def __init__(self):
         self.player = Player()
         self.moon = Moon()
-        self.factions = []
-        self.resistance = []
+        self.turn = 0
+#        self.factions = []   # now in zones
 
-    def to_json(self, filename):
-        # dump my state to a file to save it
-        raise NotImplementedError()
-
+    def json_savefile_turn(self,sdir):
+        self.json_savefile(sdir,'turn_%03d.json'%(self.turn))
+        self.json_savefile(sdir)
+    def json_savefile(self,sdir,name=None):
+        if not name:
+          name='save.json'
+        fd=open( os.path.join(sdir,name),'w' )
+        json.dump( self.json_dump(), fd, indent=2,sort_keys=True )
+        fd.close()
     @classmethod
-    def from_json(cls, filename):
-        o = cls()
-        # load my state from a file
-        raise NotImplementedError()
-        return o
+    def json_loadfile(cls,sdir,name=None):
+        if not name:
+          name='save.json'
+        jdata=json.load( open( os.path.join(sdir,name) ) )
+        return cls.json_create(jdata)
+
+    def json_dump(self):
+        v=self.json_dump_simple('turn')
+        v['player']=self.player.json_dump()
+        v['moon']=self.moon.json_dump()
+        return v
+    def json_load(self, jdata):
+        self.player=Player.json_create( jdata['player'] )
+        self.moon=Moon.json_create( jdata['moon'] )
+
 
     def update(self, ui):
         # we pass in the Game instance and the UI from the top level so the
         # model objects don't need to hang on to them
         self.player.update(self, ui)
-        for group in self.factions + self.resistance:
-            group.update(self, ui)
-        self.planet.update(self, ui)
+#        self.planet.update(self, ui)     # not defined
         self.moon.update(self, ui)
-        # tally planet threat, check for game over
-        raise NotImplementedError()
+        #
+        # save to be paranoid
+        #
+        self.json_savefile_turn( ui.savedir )
+        self.turn+=1
+        #
+        # Check for game over condition..
+        #
+        threat=0
+        for zone in self.moon.zones:
+            threat+=zone.faction.threat
+        if threat>1.2:
+          raise ui.SIGNAL_GAMEOVER
+        ui.msg('update done threat is %s'%(threat))
 
 
-class Player:
+class Player(JSONable):
     """Represents the player's orders.
 
     Use actvity points per:
@@ -44,8 +111,19 @@ class Player:
         self.visibility = 0   # affected by orders with "Instigator Noticeable"
         self.activity_points = 0
 
+    def json_dump(self):
+        return self.json_dump_simple( 'discovery_chance','visibility','activity_points' )
 
-class Moon:
+
+    def update(self, game,ui):
+        ui.msg( '%s update not implemented' % self)
+
+
+
+
+
+
+class Moon(JSONable):
     """Made up of N zones, each ruled by a Boss who has a Faction around him.
     Remote from the Planet, but controlling it economically, militarily, and
     politically. Its ability to project its power is the key to victory and so
@@ -55,12 +133,30 @@ class Moon:
     def __init__(self):
         self.zones = []
 
+    def json_dump(self):
+        v=self.json_dump_simple()
+        v['zones']=[ z.json_dump() for z in self.zones ]
+        return v
+    def json_load(self, jdata):
+        self.zones=[ Zone.json_create(z) for z in jdata['zones'] ]
+
+
     def update(self, game, ui):
+        ui.msg('%s updating moon'%(self))
         for zone in self.zones:
-            zone.update(ui)
+            zone.update(game,ui)
 
 
-class Zone:
+
+
+
+
+
+
+
+
+
+class Zone(JSONable):
     """Convert a raw material into a resource
 
     Contain two Cohorts (population groups)
@@ -68,19 +164,56 @@ class Zone:
     Utilize Servitor Cohort to carry out work
     Resource requirements must be met or dropoff in output.
     """
-    def __init__(self):
+    def __init__(self,name):
+        self.name=name
         self.requirements = []  # what raw materials are needed, how much
         self.provider = []      # what are created from what volume of inputs
         self.cohorts = [Privileged(), Servitor()]       # population groups
-        self.faction = Faction(type)
+        self.faction = None
         self.resistance_groups = []   # list of resistance groups
 
+    def json_dump(self):
+        v=self.json_dump_simple('name')
+        v['faction']=self.faction.json_dump()
+        v['cohorts.priv']=self.cohorts[0].json_dump()
+        v['cohorts.serv']=self.cohorts[1].json_dump()
+        v['resistance_groups']=[ g.json_dump() for g in self.resistance_groups ]
+        return v
+    @classmethod
+    def json_create_args(cls,jdata):
+        return [ jdata['.name'] ]
+    def json_load(self, jdata):
+        self.cohorts=[ Privileged.json_create( jdata['cohorts.priv'] ),
+                       Servitor.json_create( jdata['cohorts.serv'] ),
+                     ]
+        self.faction=Faction.json_create( jdata['faction'] )
+        self.resistance_groups=[ Resistance.json_create(g) for g in jdata['resistance_groups'] ]
+
+
     def update(self, game, ui):
+        ui.msg('%s updating zone'%(self))
+        # do plans and orders
+        for group in self.resistance_groups:
+            group.update(game, ui)
+        for group in self.cohorts:
+            group.update(game, ui)
+        self.faction.update(game, ui)
+        #
         # Process Raw Materials and create Resources
-        raise NotImplementedError('%s update not implemented' % self)
+        # (this needs work)
+        produce=0
+        for group in self.cohorts:
+            produce+= group.update_production(game,ui,self)
+        ui.msg('total produce=%s'%(produce))
+        self.faction.rich+=produce
 
 
-class Cohort:
+
+
+
+
+
+class Cohort(JSONable):
     """Each zone has at least two cohorts:
 
         Privileged - staff zone faction
@@ -97,35 +230,53 @@ class Cohort:
         self.quality_of_life = 0        # provided services
         self.cash = 0           # additional discretionary money
 
-    def update(self, game, ui):
-        raise NotImplementedError('%s update not implemented' % self)
+    def json_dump(self):
+        return self.json_dump_simple('size','liberty','quality_of_life','cash')
 
     @property
     def willing(self):
         """Willingness can be forced through low liberty, or the product of
         high quality of life and cash in combination.
         """
-        pass
+        return min( self.liberty, (self.quality_of_life+self.cash)/2 )
 
     @property
     def rebellious(self):
         """Rebellion is caused by low liberty, quality of life, and cash.
         """
-        pass
+        return min( self.liberty, self.quality_of_life, self.cash )
+
+
+    def update(self, game, ui):
+        ui.msg( '%s update not implemented' % self)
+
+    def update_production(self,game,ui,zone):
+        produce=self.willing * self.size
+        ui.msg( '%s produced: %s' %( self,produce) )
+        return produce
+
 
 
 class Privileged(Cohort):
-    pass
+    def __init__(self):
+        super(Privileged, self).__init__()
+
+
 
 
 class Servitor(Cohort):
-    pass
+    def __init__(self):
+        super(Servitor, self).__init__()
 
 
-class Group:
+
+
+
+class Group(JSONable):
     """A group of non-rabble that actually do stuff in the game.
     """
-    def __init__(self):
+    def __init__(self,name):
+        self.name=name
         self.size = 0
         self.informed = 0
         self.smart = 0
@@ -133,10 +284,21 @@ class Group:
         self.rich = 0
         self.buffs = []
 
+    def json_dump(self):
+        return self.json_dump_simple('name','size','informed','smart','loyal','rich','buffs' )
+    @classmethod
+    def json_create_args(cls,jdata):
+        return [jdata['.name']]
+
     def update(self, game, ui):
         # Groups plan - plan the action they will take next turn
         # (not visible to player)
-        raise NotImplementedError()
+        ui.msg( '%s update not implemented' % self)
+
+
+
+
+
 
 
 class Faction(Group):
@@ -146,22 +308,41 @@ class Faction(Group):
     controls a power projector (threat to the planet)
     requires support (resource or resources)
     """
-    def __init__(self):
-        super(Faction, self).__init__()
+    def __init__(self,name):
+        super(Faction, self).__init__(name)
         self.threat = 0     # potential power vs planet
+
+    def json_dump(self):
+        v=Group.json_dump(self)
+        v.update( self.json_dump_simple( 'threat' ) )
+        return v
 
     def update(self, game, ui):
         super(Faction, self).update(game, ui)
         # alter threat level against planet
-        raise NotImplementedError()
+        ui.msg( '%s update not implemented' % self)
+
+
+
+
 
 
 class Resistance(Group):
     """Resistance Group
     """
-    def __init__(self):
-        super(Resistance, self).__init__()
+    def __init__(self,name):
+        super(Resistance, self).__init__(name)
         # how obvious to the local Faction, how easy to find
         self.visibility = 0
         # style of actions to select from with chance of each
         self.modus_operandi = Plan.TYPE_ESPIONAGE
+
+    def json_dump(self):
+        v=Group.json_dump(self)
+        v.update( self.json_dump_simple('visibility','modus_operandi') )
+        return v
+
+    def update(self, game, ui):
+        super(Resistance, self).update(game, ui)
+        # alter threat level against planet
+        ui.msg( '%s update not implemented' % self)
