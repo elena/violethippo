a3d4bcb2615f9344335f6aac587aec26a29e5dda
Richard Jones <richard@mechanicalcat.net>
2013-09-02 22:44:11 +1000

    screw all that, let's use PNGs

--- game/gamelib/ui/overview.py
+++ game/gamelib/ui/overview.py
@@ -5,26 +5,17 @@ from cocos.scene import Scene
 from cocos.layer import Layer
 from cocos.sprite import Sprite
 
-import squirtle
-
 
 class Overview(Scene):
     def __init__(self):
         super(Overview, self).__init__()
-        path = os.path.join(pyglet.resource.location('overview.svg').path,
-            'overview.svg')
-        layers = squirtle.load_layers(path, circle_points=100)
-        self.add(SVG(layers['display']))
-        self.hit = layers['hitbox']
+        self.add(Display())
 
-class SVG(Layer):
-    def __init__(self, svg):
-        super(SVG, self).__init__()
-        self.svg = svg
 
-    def draw(self):
-        # pyglet.gl.glPushMatrix()
-        # self.transform()
-        self.svg.draw(0, 768)
-        # pyglet.gl.glPopMatrix()
-
+class Display(Layer):
+    def __init__(self):
+        super(Display, self).__init__()
+        self.moon = Sprite('moon.png', position=(512, 500))
+        self.add(self.moon)
+        self.planet = Sprite('planet.png', anchor=(0, 0))
+        self.add(self.planet)
