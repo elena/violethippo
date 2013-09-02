799ab678d2b2709ab161b090ff2ab91a1bd77378
Cameron Blackwood <cameron.blackwood@gmail.com>
2013-09-02 23:11:49 +1000

    Someone put non ascii in one of the python files for ' marks... bad rabbit

--- game/gamelib/player_orders.py
+++ game/gamelib/player_orders.py
@@ -9,91 +9,91 @@ class Order:
     pass
 
 
-class Hideout:
+class Hideout(Order):
     """Establish/Move hideout - select a zone, adds to time until caught
     (harder to apply support points to groups in other zones? most player
     orders only in same zone as hideout?) Note: first turn this is the
-    player’s first order, and must be done before any support is allocated
+    player's first order, and must be done before any support is allocated
     """
 
 
-class ZoneIntel:
+class ZoneIntel(Order):
     """Information on the disposition of the cohorts and the zone itself. We
     want the player to do this when they arrive (second turn) because they
     really need to know this stuff, and be worth doing every now and then
-    later on. Information on cohorts might be along the lines of “biggest
-    problem is xxx”, along with general information on their sizes and
+    later on. Information on cohorts might be along the lines of 'biggest
+    problem is xxx', along with general information on their sizes and
     functions in the zone? Perhaps several levels of info requiring several
     successive attempts (not necessarily one after another)?
     """
 
 
-class InfiltrateFaction:
+class InfiltrateFaction(Order):
     """uncover information that might be useful in working out what the
     Faction is doing this turn (an already planned action) which might enable
     the player to warn the groups through discouraging or encouraging specific
-    plans, as well as showing more detailed indication of the faction’s
+    plans, as well as showing more detailed indication of the faction's
     current status?
     """
 
 
-class LocateResistance:
+class LocateResistance(Order):
     """locate resistance groups
     """
 
 
-class Funding:
+class Funding(Order):
     """Seek funding source - prepare for Develop Group on a later turn
     """
 
 
-class DevelopGroup:
-    """Enhance a group’s stat (Smarts, Informed, Rich)
+class DevelopGroup(Order):
+    """Enhance a group's stat (Smarts, Informed, Rich)
     """
 
 
-class MakeAPlan:
+class MakeAPlan(Order):
     """Reveal a potential plan and assign it to a group (or forget it),
     perhaps select style of plan? Or presented with brief summary of three and
     you select one?
     """
 
 
-class MovePlan:
+class MovePlan(Order):
     """Take a plan from one group and give it to another (this will likely
     affect its chance of success, as the groups will be unlikely to have the
     same stats)
     """
 
 
-class Sabotage:
+class Sabotage(Order):
     """Reduce production efficiency for this turn?
     """
 
 
-class Disrupt:
+class Disrupt(Order):
     """Interdict faction plan reducing its chance of success
     """
 
 
-class Frame:
+class Frame(Order):
     """Plant evidence (situational must have evidence and conditions to make
     it relevant)
     """
 
 
-class Steal:
+class Steal(Order):
     """Steal something
     """
 
 
-class RemoveResistanceLeader:
+class RemoveResistanceLeader(Order):
     """Changes the modus operandi for the group, potentially affects its other
     stats negatively, potentially disbanding the group
     """
 
 
-class AttackFactionLeader:
+class AttackFactionLeader(Order):
     """When a Faction is weakened enough, this should become available, and
     allow the player to permanently reduce the zone/faction to providing
     minimal resources, taking no Faction actions. But also increases awareness
