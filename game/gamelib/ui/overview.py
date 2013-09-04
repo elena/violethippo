b3abbffc9509a9e28cde6dd65805bb2c56770483
Richard Jones <richard@mechanicalcat.net>
2013-09-04 21:16:33 +1000

    put all the model messages in an on-screen display (within reason; 1000 line limit)

--- game/gamelib/ui/overview.py
+++ game/gamelib/ui/overview.py
@@ -35,11 +35,13 @@ import random
 
 from cocos.director import director
 from cocos.scene import Scene
-from cocos.layer import Layer, ColorLayer
+from cocos.layer import Layer, ColorLayer, PythonInterpreterLayer
 from cocos.text import Label
 from cocos.sprite import Sprite
 from cocos.rect import Rect
 from cocos.cocosnode import CocosNode
+
+from pyglet.text.layout import IncrementalTextLayout
 
 from gamelib import model, player_orders
 import ninepatch
@@ -94,6 +96,89 @@ class ChoiceLayer(Layer):
         return True
 
 
+# we can maybe turn this on later...
+# class DebugLayer(PythonInterpreterLayer):
+#     def on_key_press(self, symbol, modifiers):
+#         if not self.visible:
+#             return False
+#         if symbol == pyglet.window.key.GRAVE:
+#             self.visible = False
+#             return True
+#         return super(DebugLayer, self).on_key_press(symbol, modifiers)
+
+#     def on_text(self, symbol):
+#         if not self.visible:
+#             return False
+#         if symbol == '`':
+#             return True
+#         return super(DebugLayer, self).on_text(symbol)
+
+#     def on_text_motion(self, motion):
+#         if not self.visible:
+#             return False
+#         return super(DebugLayer, self).on_text_motion(motion)
+
+
+# override standard pyglet DocumentLabel so we're scrollable
+# TODO batch me!
+class DocumentLabel(IncrementalTextLayout):
+    def __init__(self, document=None,
+                 x=0, y=0, width=None, height=None,
+                 anchor_x='left', anchor_y='baseline',
+                 multiline=False, dpi=None, batch=None, group=None):
+        super(DocumentLabel, self).__init__(document, width=width,
+            height=height, multiline=multiline, dpi=dpi, batch=batch,
+            group=group)
+        self._x = x
+        self._y = y
+        self._anchor_x = anchor_x
+        self._anchor_y = anchor_y
+        self._update()
+
+    def _get_text(self):
+        return self.document.text
+    def _set_text(self, text):
+        self.document.text = text
+    text = property(_get_text, _set_text)
+
+
+class DebugLayer(Layer):
+    is_event_handler = True
+    def __init__(self):
+        super(DebugLayer, self).__init__()
+        w, h = director.get_window_size()
+        self.add(ColorLayer(245, 244, 240, 200), z=-1)
+        self.lines = ["hi, I'm the console!\n"]
+        self.document = pyglet.text.decode_text('')
+        self.label = DocumentLabel(self.document, width=w-20, height=h-20,
+             y=h-10, x=10, anchor_y='top', multiline=True)
+
+    def draw(self):
+        self.label.draw()
+
+    def write(self, text):
+        self.lines.append(text)
+        if len(self.lines) > 1000:
+            self.lines.pop(0)
+        if self.visible:
+            self.label.text = '\n'.join(self.lines)
+            self.label.ensure_line_visible(self.label.get_line_count()-1)
+
+    def toggle_visible(self):
+        self.visible = not self.visible
+        if self.visible:
+            self.label.text = '\n'.join(self.lines)
+            self.label.ensure_line_visible(self.label.get_line_count()-1)
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.GRAVE:
+            self.toggle_visible()
+            return True
+
+    def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
+        self.label.view_x -= scroll_x
+        self.label.view_y += scroll_y * 16
+
 class Fixed(Layer):   # "display" needs to be renamed "the one with buttons and info"
     is_event_handler = True
 
@@ -122,6 +207,10 @@ class Fixed(Layer):   # "display" needs to be renamed "the one with buttons and 
 
         self.zone = Zone()
         self.add(self.zone)
+
+        self.console = DebugLayer()
+        self.add(self.console, z=10)
+        self.console.visible = False
 
     def update(self):
         self.turn_label.element.text = 'Turn: %d\nActivity_points: %d\nHideout: %s' % (
@@ -161,6 +250,11 @@ class Fixed(Layer):   # "display" needs to be renamed "the one with buttons and 
             if action.rect.contains(x, y):
                 action.order.execute(self)
 
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.GRAVE:
+            self.console.toggle_visible()
+            return True
+
     def ask_choice(self, title, choices, callback):
         '''A player order wants us to ask the user to make a choice.
 
@@ -169,8 +263,7 @@ class Fixed(Layer):   # "display" needs to be renamed "the one with buttons and 
         self.add(ChoiceLayer(title, choices, callback), z=1)
 
     def msg(self, message, *args):
-        # TODO log this crap in something on-screen
-        print message % args
+        self.console.write(message % args)
 
     class SIGNAL_GAMEOVER(Exception):
         pass
