b9a65723cd412de9975c1db9c42930cde8f02346
Richard Jones <richard@mechanicalcat.net>
2013-09-07 11:53:32 +1000

    whoops

---
+++ game/gamelib/chance.py
@@ -0,0 +1,44 @@
+from random import random
+
+EASE_LINEAR=0
+EASE_HERMITE=1
+EASE_QUAD=2
+EASE_CUBIC=3
+
+def roll(d1, d2=0.0):
+    total = d1+d2
+    if total >= 1.:
+        return max(random(), random()) + (total - 1.)
+    #
+    # Use if want to extend optimal requirements (lower chance of success)
+    # if total > 1.5:
+    #     return total - .5
+    # total *= 2./3.
+    #
+    total = ease(total)
+    ran = random()
+    return max(0, total-ran)
+
+def ease(total, ease=EASE_CUBIC):
+    if total > 2:
+        raise Exception('EasingValueTooLarge')
+    if total > 1:
+        return ease(total-1, ease) + 1
+    if ease == EASE_LINEAR:
+        return total
+    elif ease == EASE_HERMITE:
+        return (3 * total**2) - (2 * total**3)
+    elif ease == EASE_QUAD:
+        total *= 2
+        if total < 1:
+            return .5 * total**2
+        total -= 1
+        return -.5 * (total*(total-2) - 1)
+    elif ease == EASE_CUBIC:
+        total *= 2
+        if total < 1:
+           return .5 * total**3
+        total -= 2
+        return .5 * (total**3 + 2)
+    else:
+        return 0
