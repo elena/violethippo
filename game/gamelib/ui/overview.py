39b2c3a199b8ad37fe0ada6772c2af687de93019
Richard Jones <richard@mechanicalcat.net>
2013-09-07 18:27:09 +1000

    clean up info displays and use graphs for number display; start to move away from instance attributes and use cocos named elements

--- game/gamelib/ui/overview.py
+++ game/gamelib/ui/overview.py
@@ -80,35 +80,32 @@ class Fixed(Layer):   # "display" needs to be renamed "the one with buttons and 
         w, h = director.get_window_size()
 
         # now it's safe to show information about the game
-        self.threat_label = Label('Threat:', x=450, y=680, anchor_y='top',
-            font_name='Prototype')
-        self.add(self.threat_label)
-        self.threat_graph = MultipleBargraph((200, 8), (.1, .1, .1),
-            position=(260, 332), style='solid',
-            colors=[(200, 100, 100), (200, 200, 100), (100, 200, 100)])
-        self.add(self.threat_graph)
-        self.visible_label = Label('Visible:', x=450, y=660, anchor_y='top',
-            font_name='Prototype')
-        self.add(self.visible_label, z=1)
-        self.visible_graph = MultipleBargraph((200, 8), (1, 1, 1),
-            position=(260, 322), style='solid',
-            colors=[(200, 100, 100), (200, 200, 100), (100, 200, 100)])
-        self.add(self.visible_graph, z=1)
+        self.add(Label('Threat:', x=450, y=680, anchor_y='top',
+            font_name='Prototype'), 'threat')
+        self.add(MultipleBargraph((200, 8), (.1, .1, .1), position=(260, 332),
+            style='solid', colors=[(200, 100, 100), (200, 200, 100),
+            (100, 200, 100)]), name='threat_graph')
+        self.add(Label('Visible:', x=450, y=660, anchor_y='top',
+            font_name='Prototype'))
+        self.add(MultipleBargraph((200, 8), (1, 1, 1), position=(260, 322),
+            style='solid', colors=[(200, 100, 100), (200, 200, 100),
+            (100, 200, 100)]), name='visible_graph')
 
-        self.turn_label = Label('', multiline=True, x=20, y=70, width=200,
-            anchor_x='left', anchor_y='bottom', font_name='Prototype')
-        self.add(self.turn_label)
+        self.add(Label('', multiline=True, x=20, y=70, width=200,
+            anchor_x='left', anchor_y='bottom', font_name='Prototype'),
+            name='turn_label')
 
-        self.end_turn = Button('end turn button.png', (w-32, h-32), None,
+        end_turn = Button('end turn button.png', (w-32, h-32), None,
             self.on_new_turn)
-        self.buttons.append(self.end_turn)
-        self.add(self.end_turn)
+        self.buttons.append(end_turn)
+        self.add(end_turn, name='end_turn')
 
-        self.info = Info()
+        self.info = Details()
+        self.info.position = (0, 300)
         self.add(self.info)
 
         self.zinfo = ZoneInfo()
-        self.zinfo.position = (300, h-300)
+        self.zinfo.position = (400, h-130)
         self.add(self.zinfo)
 
         self.zone = Zone()
@@ -123,24 +120,26 @@ class Fixed(Layer):   # "display" needs to be renamed "the one with buttons and 
         free = ''
         if model.game.player.free_order:
             free = ' + free'
-        self.turn_label.element.text = 'Turn: %d\nActivity_points: %d%s\nHideout: %s' % (
-            model.game.turn, model.game.player.activity_points, free,
-            model.game.player.hideout or 'Not Chosen')
+        self.get('turn_label').element.text = 'Turn: %d\nActivity Points: '\
+            '%d%s\nHideout: %s' % (model.game.turn,
+                model.game.player.activity_points, free,
+                model.game.player.hideout or 'Not Chosen')
+
         zone1 = model.game.moon.zones[model.INDUSTRY]
         zone2 = model.game.moon.zones[model.LOGISTICS]
         zone3 = model.game.moon.zones[model.MILITARY]
-        self.threat_graph.values = (zone1.faction.threat, zone2.faction.threat,
-            zone3.faction.threat)
-        self.visible_graph.values = (zone1.player_found,
+        self.get('threat_graph').values = (zone1.faction.threat,
+            zone2.faction.threat, zone3.faction.threat)
+        self.get('visible_graph').values = (zone1.player_found,
             zone2.player_found, zone3.player_found)
         self.zinfo.display_zone(self.zone.mode)
 
         if not model.game.player.hideout:
             # show Establish/Move hideout button only
-            self.end_turn.visible = False
+            self.get('end_turn').visible = False
         else:
             # enable turn button and all the player actions
-            self.end_turn.visible = True
+            self.get('end_turn').visible = True
 
         # remove old player order buttons
         for but in list(self.buttons):
@@ -330,32 +329,53 @@ class Zone(Layer):
             self.parent.info.show_resistance(but.info)
 
 
-class Info(Layer):
+class InfoLayer(Layer):
+    def packer(self):
+        def add_info():
+            self.height = 0
+            self.min_x = self.max_x = 0
+            while 1:
+                text, info, value = yield
+                y = -self.height
+                label = Label(text, align='right', anchor_x='right',
+                    anchor_y='top', x=0, y=y, font_name='Prototype')
+                self.min_x = min(self.min_x, -label.element.content_width)
+                self.height += label.element.content_height
+                self.add(label)
+                if info is not None:
+                    label = Label(info, align='left', anchor_x='left',
+                        anchor_y='top', x=2, y=y, font_name='Prototype')
+                    self.add(label)
+                    self.max_x = max(self.max_x, label.element.content_width)
+                else:
+                    self.bg = Bargraph((80, 10), value, position=(2, y-16))
+                    self.max_x = max(self.max_x, 80)
+                    if value < .3:
+                        self.bg.color = (200, 100, 100)
+                    elif value < .7:
+                        self.bg.color = (200, 200, 100)
+                    else:
+                        self.bg.color = (100, 200, 100)
+                    self.add(self.bg)
+                self.width = self.max_x - self.min_x
+
+        adder = add_info()
+        adder.next()
+        add_info = lambda *a: adder.send(a)
+        return add_info
+
+
+class Details(InfoLayer):
     is_event_handler = True
-    def __init__(self):
-        super(Info, self).__init__()
-
-        self.info_label = Label('', multiline=True,
-            width=350, anchor_x='left', anchor_y='bottom', x=10, y=250,
-            font_name='Prototype')
-        self.info_label.visible = False
-
-        self.popup_9p = LabelNinepatch('border-9p.png', self.info_label)
-        self.add(self.popup_9p)
-        self.popup_9p.visible = False
-
-        self.active_info = None
+    active_info = None
 
     def on_mouse_press(self, x, y, button, modifiers):
-        if self.popup_9p.visible and self.popup_9p.rect.contains(x, y):
-            self.info_label.element.text = ''
-            self.info_label.visible = False
-            self.popup_9p.visible = False
+        if self.visible and self.rect.contains(x, y):
+            self.visible = False
             return True         # event handled
 
     def hide_info(self):
-        self.info_label.visible = False
-        self.popup_9p.visible = False
+        self.visible = False
         self.active_info = None
 
     def show_faction(self, active_zone):
@@ -365,37 +385,33 @@ class Info(Layer):
             self.hide_info()
             return
         self.active_info = faction
-        self.info_label.element.text = '\n'.join([
-            'Faction: %s' % active_zone,
-            'Leader: %s' % faction.name,
-            'Size: %.1f' % (faction.size * 10),
-            'Informed: %.1f' % (faction.informed * 10),
-            'Smart: %.1f' % (faction.smart * 10),
-            'Loyal: %.1f' % (faction.loyal * 10),
-            'Rich: %.1f' % (faction.rich * 10),
-            'Buffs: %s' % ', '.join(faction.buffs),
-            'Threat: %.1f' % (faction.threat * 10),
-            'Alert: %.2f' % (faction.alert,),
+        self.show_info([
+            ('Faction:', active_zone, None),
+            ('Leader:', faction.name, None),
+            ('Size:', None, faction.size),
+            ('Informed:', None, faction.informed),
+            ('Smart:', None, faction.smart),
+            ('Loyal:', None, faction.loyal),
+            ('Rich:', None, faction.rich),
+            ('Buffs:', ', '.join(faction.buffs), None),
+            ('Threat:', None, faction.threat),
+            ('Alert:', None, faction.alert),
         ])
-        self.info_label.visible = True
-        self.popup_9p.visible = True
 
     def show_resistance(self, group):
         if self.active_info == group:
             self.hide_info()
             return
         self.active_info = group
-        self.info_label.element.text = '\n'.join([
-            'Name: %s' % group.name,
-            'Size: %.1f' % (group.size * 10),
-            'Informed: %.1f' % (group.informed * 10),
-            'Smart: %.1f' % (group.smart * 10),
-            'Rich: %.1f' % (group.rich * 10),
-            'Loyal: %.1f' % (group.loyal * 10),
-            'Buffs: %s' % (', '.join(group.buffs),)
+        self.show_info([
+            ('Name:', group.name, None),
+            ('Size:', None, group.size),
+            ('Informed:', None, group.informed),
+            ('Smart:', None, group.smart),
+            ('Rich:', None, group.rich),
+            ('Loyal:', None, group.loyal),
+            ('Buffs:', ', '.join(group.buffs), None),
         ])
-        self.info_label.visible = True
-        self.popup_9p.visible = True
 
     def show_cohort(self, cohort):
         if self.active_info == cohort:
@@ -403,23 +419,36 @@ class Info(Layer):
             return
         self.active_info = cohort
         if cohort.is_servitor:
-            output = 'Willingness: %.1f' % (cohort.willing * 10)
+            output = ('Willingness:', None, cohort.willing)
         else:
-            output = 'Efficiency: %.1f' % (cohort.efficiency * 10)
-        self.info_label.element.text = '\n'.join([
-            'Cohort: %s' % cohort.__class__.__name__,
-            'Size: %.1f' % (cohort.size * 10),
-            'Liberty: %.1f' % (cohort.liberty * 10),
-            'Quality of Life: %.1f' % (cohort.quality_of_life * 10),
-            'Cash: %.1f' % (cohort.cash * 10),
+            output = ('Efficiency:', None, cohort.efficiency)
+        self.show_info([
+            ('Cohort:', cohort.__class__.__name__, None),
+            ('Size:', None, cohort.size),
+            ('Liberty:', None, cohort.liberty),
+            ('Quality of Life:', None, cohort.quality_of_life),
+            ('Cash:', None, cohort.cash),
             output,
-            'Rebelliousness: %.1f' % (cohort.rebellious * 10),
+            ('Rebelliousness:', None, cohort.rebellious),
         ])
-        self.info_label.visible = True
-        self.popup_9p.visible = True
+
+    def show_info(self, info):
+        for z, child in list(self.children):
+            self.remove(child)
+
+        packer = self.packer()
+        for row in info:
+            packer(*row)
+
+        self.x = -self.min_x + 10
+#        self.y = self.height
+
+        # self.popup_9p = LabelNinepatch('border-9p.png', self)
+        # self.add(self.popup_9p)
+        self.visible = True
 
 
-class ZoneInfo(Layer):
+class ZoneInfo(InfoLayer):
     descr = dict(
         industry='produces goods and food',
         logistics='transports to and from planet',
@@ -430,46 +459,21 @@ class ZoneInfo(Layer):
         for z, child in list(self.children):
             self.remove(child)
 
-        def add_info():
-            self.height = 0
-            while 1:
-                text, info, value = yield
-                y = -self.height
-                label = Label(text, align='right', anchor_x='right',
-                    anchor_y='top', x=0, y=y, font_name='Prototype')
-                self.height += label.element.content_height
-                self.add(label)
-                if info:
-                    self.add(Label(info, align='left', anchor_x='left',
-                        anchor_y='top', x=2, y=y, font_name='Prototype'))
-                else:
-                    self.bg = Bargraph((80, 10), value, position=(2, y-16))
-                    if value < .3:
-                        self.bg.color = (200, 100, 100)
-                    elif value < .7:
-                        self.bg.color = (200, 200, 100)
-                    else:
-                        self.bg.color = (100, 200, 100)
-                    self.add(self.bg)
-
-        adder = add_info()
-        adder.next()
-        add_info = lambda *a: adder.send(a)
-
         zone = model.game.moon.zones[active_zone]
 
-        add_info('Zone', self.descr[active_zone], None)
-        add_info('Provides:', ', '.join(zone.provides), None)
-        add_info('Requires:', ', '.join(zone.requirements), None)
+        packer = self.packer()
+        packer('Zone', self.descr[active_zone], None)
+        packer('Provides:', ', '.join(zone.provides), None)
+        packer('Requires:', ', '.join(zone.requirements), None)
         for n in zone.store:
-            add_info('%s:' % n.title(), '', zone.store[n] / 10.)
-        # add_info('Privileged Rebellious:', '', zone.privileged.rebellious)
-        # add_info('Servitor Rebellious:', '', zone.servitor.rebellious)
-        # add_info('Privileged Willing:', '', zone.privileged.willing)
-        # add_info('Servitor Willing:', '', zone.servitor.willing)
-        add_info('Faction Status:', zone.faction.state_description, '')
+            packer('%s:' % n.title(), None, zone.store[n] / 10.)
+        # packer('Privileged Rebellious:', None, zone.privileged.rebellious)
+        # packer('Servitor Rebellious:', None, zone.servitor.rebellious)
+        # packer('Privileged Willing:', None, zone.privileged.willing)
+        # packer('Servitor Willing:', None, zone.servitor.willing)
+        packer('Faction Status:', zone.faction.state_description, None)
 
-        self.anchor_y = -self.height
+        self.width = self.max_x - self.min_x
 
 if __name__ == '__main__':
     pyglet.resource.path.append('../../data')
