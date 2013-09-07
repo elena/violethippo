e4e55aa63cc958b69b3482e89219a72526ba33e5
Cameron Blackwood <cameron.blackwood@gmail.com>
2013-09-07 19:59:33 +1000

    who the bloody hell has functions and args called the same thing and then expects ease(n,ease) to work?

--- game/gamelib/chance.py
+++ game/gamelib/chance.py
@@ -19,22 +19,22 @@ def roll(d1, d2=0.0):
     ran = random()
     return max(0, total-ran)
 
-def ease(total, ease=EASE_CUBIC):
+def ease(total, easetype=EASE_CUBIC):
     if total > 2:
         raise Exception('EasingValueTooLarge')
     if total > 1:
-        return ease(total-1, ease) + 1
-    if ease == EASE_LINEAR:
+        return ease(total-1, easetype) + 1
+    if easetype == EASE_LINEAR:
         return total
-    elif ease == EASE_HERMITE:
+    elif easetype == EASE_HERMITE:
         return (3 * total**2) - (2 * total**3)
-    elif ease == EASE_QUAD:
+    elif easetype == EASE_QUAD:
         total *= 2
         if total < 1:
             return .5 * total**2
         total -= 1
         return -.5 * (total*(total-2) - 1)
-    elif ease == EASE_CUBIC:
+    elif easetype == EASE_CUBIC:
         total *= 2
         if total < 1:
            return .5 * total**3
